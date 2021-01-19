4e04864be4c9f52ba831a6f976d5ce3b569dbc23
Hervé Saint-Amand <git@saintamh.org>
2021-01-19 21:50:23 +0000

    Add html.py

--- klon/__init__.py
+++ klon/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 from .build import build_etree
+from .html import make_all_urls_absolute
 from .utils import is_etree, tostring
---
+++ klon/html.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# standards
+from itertools import chain
+from urllib.parse import urljoin
+
+# 3rd parties
+import lxml.etree as ET
+
+
+TAGS_WITH_URL_ATTRIBUTES = {
+    'a': 'href',
+    'area': 'href',
+    'audio': 'src',
+    'base': 'href',
+    'blockquote': 'cite',
+    'del': 'cite',
+    'embed': 'src',
+    'form': 'action',
+    'frame': 'src',
+    'iframe': 'src',
+    'img': 'src',
+    'input': 'src',
+    'ins': 'cite',
+    'link': 'href',
+    'object': 'data',
+    'script': 'src',
+    'source': 'src',
+    'track': 'src',
+    'video': 'src',
+}
+
+
+XPATH_TAGS_WITH_URL_ATTRIBUTES = '*[%s]' % ' or '.join(
+    f'self::{tag}' for tag in sorted(TAGS_WITH_URL_ATTRIBUTES)
+)
+
+
+def parse_html_etree(html_str):
+    return ET.HTML(html_str)
+
+
+def make_all_urls_absolute(base_url, etree):
+    """
+    Modify all links in the given HTML etree to be absolute URLs, using the given `base_url` to resolve relative URLs.
+    """
+    print(XPATH_TAGS_WITH_URL_ATTRIBUTES)
+    for node in chain([etree], etree.xpath(XPATH_TAGS_WITH_URL_ATTRIBUTES)):
+        attr = TAGS_WITH_URL_ATTRIBUTES.get(node.tag)
+        value = attr and node.get(attr)
+        if value is not None:
+            absolute_url = urljoin(base_url, value)
+            if absolute_url != value:
+                node.set(attr, absolute_url)
