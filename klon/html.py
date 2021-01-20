b1ab10bd099cb4ec1b6c50c92146fb24a09e5ca2
Hervé Saint-Amand <git@saintamh.org>
2021-01-20 08:48:13 +0000

    Refactor logic

--- klon/html.py
+++ klon/html.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # standards
-from itertools import chain
 from urllib.parse import urljoin
 
 # 3rd parties
@@ -31,7 +30,7 @@ TAGS_WITH_URL_ATTRIBUTES = {
 }
 
 
-XPATH_TAGS_WITH_URL_ATTRIBUTES = '*[%s]' % ' or '.join(
+XPATH_TAGS_WITH_URL_ATTRIBUTES = '//*[%s]' % ' or '.join(
     f'self::{tag}' for tag in sorted(TAGS_WITH_URL_ATTRIBUTES)
 )
 
@@ -45,9 +44,9 @@ def make_all_urls_absolute(base_url, etree):
     Modify all links in the given HTML etree to be absolute URLs, using the given `base_url` to resolve relative URLs.
     """
     print(XPATH_TAGS_WITH_URL_ATTRIBUTES)
-    for node in chain([etree], etree.xpath(XPATH_TAGS_WITH_URL_ATTRIBUTES)):
-        attr = TAGS_WITH_URL_ATTRIBUTES.get(node.tag)
-        value = attr and node.get(attr)
+    for node in etree.xpath(XPATH_TAGS_WITH_URL_ATTRIBUTES):
+        attr = TAGS_WITH_URL_ATTRIBUTES[node.tag]
+        value = node.get(attr)
         if value is not None:
             absolute_url = urljoin(base_url, value)
             if absolute_url != value:
