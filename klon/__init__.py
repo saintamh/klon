5eff55cfd59e80cf9142fb5e67fd2c1a3b586dc5
Hervé Saint-Amand <git@saintamh.org>
2021-07-05 07:08:06 +0100

    Add `parse_xml_etree`

--- klon/__init__.py
+++ klon/__init__.py
@@ -5,3 +5,4 @@ from .forms import parse_form
 from .html import extract_js_str, make_all_urls_absolute, parse_html_etree
 from .text import extract_multiline_text, extract_text, normalize_spaces
 from .utils import Element, detach, is_element, tostring
+from .xml import parse_xml_etree
